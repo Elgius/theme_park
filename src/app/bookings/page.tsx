f2ccbf034f278adaceb05e0b2b69d74c0debcf56
Elgius <114131653+Elgius@users.noreply.github.com>
2024-10-17 20:19:09 +0500

    cleared build errors

--- src/app/bookings/page.tsx
+++ src/app/bookings/page.tsx
@@ -19,10 +19,8 @@ import {
   ModalTrigger,
 } from "@/components/ui/animated-modal";
 import { motion } from "framer-motion";
-import { useRouter } from "next/navigation";
 
 export default function Page() {
-  const router = useRouter();
   const bookingLink = "/date";
   const images = [
     "https://images.unsplash.com/photo-1517322048670-4fba75cbbb62?q=80&w=3000&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
