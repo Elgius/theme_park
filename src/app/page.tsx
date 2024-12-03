9c85623f7fabb0eea5d91f81a3f774b4c2076155
Elgius <114131653+Elgius@users.noreply.github.com>
2024-12-03 13:13:38 +0500

    autodeploy pt2

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -7,7 +7,7 @@ export default function Home() {
   return (
     <div className="grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20 font-[family-name:var(--font-geist-sans)] bg-gradient-to-b from-blue-300 to-green-500">
       <main className="flex flex-col gap-8 row-start-2 items-center sm:items-start">
-        <div>We in business</div>
+        <div>We in business, auto-deploy mode on</div>
         <div>
           <Link href="/main">
             <Button>Version 3.0</Button>
