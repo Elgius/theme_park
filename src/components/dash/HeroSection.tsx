bad3cd184b7deddd45baa3cfd1388cbcb992506c
Elgius <114131653+Elgius@users.noreply.github.com>
2024-12-14 03:36:36 +0500

    fixed build errors

--- src/components/dash/HeroSection.tsx
+++ src/components/dash/HeroSection.tsx
@@ -1,9 +1,9 @@
-// import Image from "next/image";
+import Image from "next/image";
 
 export function HeroSection() {
   return (
     <div className="relative h-[50vh] w-full overflow-hidden rounded-xl">
-      <img
+      <Image
         src="https://images.unsplash.com/photo-1697797738602-4f9ff59c7ac4?q=80&w=1887&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D"
         alt="Theme Park"
         layout="fill"
